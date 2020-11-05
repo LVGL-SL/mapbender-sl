2ebb149b5e0d8d1da34dddf296960e9e13084e6b
Sven van Crombrugge <s.vancrombrugge@lvgl.saarland.de>
2020-11-05 12:43:07 +0100

    Revert "Merge branch 'master' of upstream"
    This reverts commit 76ce51100c145a0350d80f71bac2f78988b6d359, reversing
    changes made to 076e5309d37705be390888d3bb81ffafa940e474.

--- http/php/mod_layerISOMetadata.php
+++ http/php/mod_layerISOMetadata.php
@@ -757,7 +757,7 @@ SQL;
 	$DCP = $iso19139->createElement ( "srv:DCP" );
 	$DCPList = $iso19139->createElement ( "srv:DCPList" );
 	$DCPList->setAttribute ( "codeList", "http://standards.iso.org/ittf/PubliclyAvailableStandards/ISO_19139_Schemas/resources/codelist/ML_gmxCodelists.xml#DCPList" );
-	$DCPList->setAttribute ( "codeListValue", "WebServices" );
+	$DCPList->setAttribute ( "codeListValue", "WebService" );
 	
 	$DCP->appendChild ( $DCPList );
 	
