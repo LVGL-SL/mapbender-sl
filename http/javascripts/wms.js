f5ea99952c3385eea82d607e0a9be318d657815e
Sven van Crombrugge <s.vancrombrugge@lvgl.saarland.de>
2021-06-14 15:29:22 +0200

    fix handling of parent layers

--- http/javascripts/wms.js
+++ http/javascripts/wms.js
@@ -617,6 +617,7 @@ wms_const.prototype.handleLayer = function(layer_name, type, value){
 	var found = false;
 	for(i = 0; i < this.objLayer.length; i++){
 		if(this.objLayer[i].layer_name==layer_name) {
+			this.objLayer[i].gui_layer_visible = parseInt(value, 10);
 			found = true;
 			break;
 		}
@@ -627,10 +628,10 @@ wms_const.prototype.handleLayer = function(layer_name, type, value){
 	}
 	
 	//Set visibility/queryability of Layer and Sublayers
-	for(var j = i; j < this.objLayer.length; j++){
-		if (i != j && this.objLayer[i].layer_parent >= this.objLayer[j].layer_parent) {
-			break;
-		}
+	for(var j = 0; j < this.objLayer.length; j++){
+		if (i == j) continue;
+		if (this.objLayer[j].layer_parent != this.objLayer[i].layer_pos) continue;
+
 		if(type == "visible") {
 			this.objLayer[j].gui_layer_visible = parseInt(value, 10);
 		}
