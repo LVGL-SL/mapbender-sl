cee534130630dee0de60e2f256c471f4a540b204
lvgl-cs <c.sommer@lvgl.saarland.de>
2023-10-18 11:38:04 +0200

    Ticket 6122 - Rerolled a change for gui altlastenktaster to open directly without resetting the map client because it can also be done in the gui administration

--- http/frames/index.php
+++ http/frames/index.php
@@ -38,13 +38,6 @@ $minify = true;
 //new for geoportal.rlp - some guis has special functions - for normal mapbender installation this doesn't matter
 if (Mapbender::session()->get("mb_user_gui") !== false) {
 	Mapbender::session()->set("previous_gui",Mapbender::session()->get("mb_user_gui"));
-	if (((Mapbender::session()->get("mb_user_gui") != "Altlastenkataster") && ($gui_id == "Altlastenkataster")) || ((Mapbender::session()->get("mb_user_gui") != "Lärmkartierung-2017") && ($gui_id == "Lärmkartierung-2017"))){
-		if (Mapbender::session()->exists("mb_wmc")) {
-			Mapbender::session()->delete("mb_wmc");
-			$e = new mb_exception("\n\nDelete mb_wmc\n\n");
-	    }
-		
-    }
 }
 Mapbender::session()->set("mb_user_gui",$gui_id);
 //
