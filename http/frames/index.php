2866c7081480c729b5e6274fc546c10ee74ee461
karlbrink <karl.owesny@csoffm.de>
2025-01-31 08:10:15 +0100

    bugfixing and revert doctype

--- http/frames/index.php
+++ http/frames/index.php
@@ -117,8 +117,9 @@ if (isset($_REQUEST["minify"]) & $_REQUEST["minify"] != "") {
 $linebreak = "\n";
 if ($representationType == "htmlComplete") {
     $html = "";
-    //$html .= "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">".$linebreak;
-	$html .= "<!DOCTYPE HTML>".$linebreak;
+    $html .= "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">".$linebreak;
+	//$html .= "<!DOCTYPE HTML>".$linebreak;
+	//DOCTYPE html leads to miscalculated heights of jquery dialogs and therefor places vertical scrollbars
     $html .= "<html>".$linebreak;
     $html .= "<head>".$linebreak;
     $html .= "<!-- Licensing: See the GNU General Public License for more details. http://www.gnu.org/copyleft/gpl.html or: mapbender/licence/ -->".$linebreak;
