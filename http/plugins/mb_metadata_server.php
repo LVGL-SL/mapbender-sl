c1f44e7f2119cb954a745f0e2229e96f0222ce04
Ole Harth <o.harth@lvgl.saarland.de>
2025-05-26 13:45:05 +0200

    replace create_function() calls (removed in PHP 8)

--- http/plugins/mb_metadata_server.php
+++ http/plugins/mb_metadata_server.php
@@ -195,7 +195,7 @@ SQL;
 		);
 		while ($row = db_fetch_row($res)) {
 			// convert NULL to '', NULL values cause datatables to crash
-			$walk = array_walk($row, create_function('&$s', '$s=strval($s);'));
+			$row = array_map('strval', $row);
 			$resultObj["data"][]= $row;
 		}
 		$ajaxResponse->setResult($resultObj);
@@ -220,7 +220,7 @@ SQL;
 		);
 		while ($row = db_fetch_row($res)) {
 			// convert NULL to '', NULL values cause datatables to crash
-			$walk = array_walk($row, create_function('&$s', '$s=strval($s);'));
+			$row = array_map('strval', $row);
 			$resultObj["data"][]= $row;
 		}
 		$ajaxResponse->setResult($resultObj);
